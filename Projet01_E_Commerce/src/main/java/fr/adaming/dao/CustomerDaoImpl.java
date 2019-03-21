1975b00a0652773e7a9bc97e5d9ce25131a38943
IN-BR-009 <IN-BR-009@IN-BR-009>
2019-03-21 10:11:17 +0100

    youhiii

--- Projet01_E_Commerce/src/main/java/fr/adaming/dao/CustomerDaoImpl.java
+++ Projet01_E_Commerce/src/main/java/fr/adaming/dao/CustomerDaoImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import fr.adaming.model.Customer;
-import fr.adaming.model.Etudiant;
 
 @Repository
 public class CustomerDaoImpl implements ICustomerDao {
